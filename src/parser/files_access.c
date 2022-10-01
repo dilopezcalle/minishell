999eb8b7c25312b8d06e9d14a5567a1e4cb2356b
Diego Lopez calle <dilopez-@c3r5s4.42urduliz.com>
2022-10-01 16:12:56 +0200

    parser

---
+++ src/parser/files_access.c
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   files_access.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dilopez- <dilopez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 16:52:00 by dilopez-          #+#    #+#             */
+/*   Updated: 2022/09/27 08:15:34 by dilopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "files_access.h"
+
+// Guardar el fd de un fichero output dependiendo de su tipo de redirección
+int	check_access_outfile(t_simple_command *command, char *file_name)
+{
+	int	fd;
+
+	fd = -1;
+	if (command->redirection_outfile == 1)
+		fd = open(file_name, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	else
+		fd = open(file_name, O_RDWR | O_APPEND | O_CREAT, 0644);
+	if (fd < 0)
+	{
+		printf("minishell: %s: %s\n", file_name, strerror(2));
+		command->outfile = -1;
+		return (1);
+	}
+	if (command->outfile != 1)
+		close(command->outfile);
+	command->outfile = fd;
+	return (0);
+}
+
+// Guardar el fd de un fichero input dependiendo de su tipo de redirección
+int	check_access_infile(t_simple_command *command, char *file_name)
+{
+	int		fd;
+	char	*aux;
+	char	*join;
+
+	fd = -1;
+	if (command->redirection_infile == 2)
+	{
+		command->infile = 0;
+		join = readline_infile(file_name);
+		if (!join)
+			return (1);
+		dub_stdin_infile(command, join);
+		return (0);
+	}
+	fd = open(file_name, O_RDONLY);
+	if (fd < 0)
+	{
+		printf("minishell: %s: %s\n", file_name, strerror(errno));
+		command->infile = -1;
+		return (1);
+	}
+	if (command->infile != 0)
+		close(command->infile);
+	command->infile = fd;
+	return (0);
+}
+
+// En el caso de ("<<"), leer desde la entrada estándar y guardar string
+static char	*readline_infile(char *end_word)
+{
+	char	*actual;
+	char	*join;
+	char	*aux;
+
+	aux = readline("> ");
+	join = 0;
+	while (aux && ft_strncmp(aux, end_word, ft_strlen(end_word) + 1) != 0)
+	{
+		actual = ft_strjoin(aux, "\n");
+		free(aux);
+		if (!join)
+			aux = ft_substr(actual, 0, ft_strlen(actual));
+		else
+		{
+			aux = ft_strjoin(join, actual);
+			free(join);
+		}
+		join = ft_substr(aux, 0, ft_strlen(aux));
+		free(actual);
+		free(aux);
+		aux = readline("> ");
+	}
+	free(aux);
+	return (join);
+}
+
+// En el caso de ("<<"), guardar el string estándar en un fd
+static void	dub_stdin_infile(t_simple_command *command, char *join)
+{
+	int		fd_pipe[2];
+	int		pid;
+
+	if (pipe(fd_pipe) == -1)
+	{
+		perror("minishell: pipe");
+		exit(1);
+	}
+	pid = fork_error_infile();
+	if (!pid)
+	{
+		close(fd_pipe[0]);
+		dup2(fd_pipe[1], 1);
+		close(fd_pipe[1]);
+		write(1, join, ft_strlen(join));
+		free(join);
+		exit(0);
+	}
+	signal(SIGINT, sig_handler);
+	close(fd_pipe[1]);
+	wait(NULL);
+	command->infile = fd_pipe[0];
+}
+
+// Comprobar que el fork no de error (norminette)
+static int	fork_error_infile(void)
+{
+	int	pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("minishell: fork");
+		exit(1);
+	}
+	return (pid);
+}
---
+++ src/parser/lexer.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almirand <almirand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/18 09:34:54 by dilopez-          #+#    #+#             */
+/*   Updated: 2022/10/01 10:05:33 by almirand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+
+// Separar en "argumentos" los comandos con sus argumentos, el input y el output
+char	**lexer(char *command_line, char	**envp)
+{
+	char	**separate_line;
+	char	**command_args;
+	int		size;
+	int		i;
+
+	size = 1;
+	i = -1;
+	separate_line = tokens(command_line, envp);
+	free(command_line);
+	if (check_syntax_errors(separate_line))
+		return (free_double_array((void **)separate_line), NULL);
+	while (separate_line[++i])
+	{
+		if (separate_line[i][0] == '|' || separate_line[i][0] == '>' \
+			|| separate_line[i][0] == '<')
+			size += 2;
+		if (i > 2 && (separate_line[i - 2][0] == '>' \
+			|| separate_line[i - 2][0] == '<'))
+			size++;
+	}
+	command_args = (char **)ft_calloc((size + 1), sizeof(char *));
+	if (!command_args \
+		|| mix_args_command(&command_args, separate_line, -1) == -1)
+		return (0);
+	return (free_double_array((void **)separate_line), command_args);
+}
+
+//Comprueba si hay un >, < ó | que represente el final de cada comando / archivo
+static int	mix_args_command(char ***command_args, char **separate, int i)
+{
+	int	end;
+	int	start;
+
+	start = 0;
+	while (separate[++i])
+	{
+		end = check_end_command(separate, i, &start);
+		if (end != -1 && start <= end)
+		{
+			if (append_command_array(command_args, separate, start, end))
+				return (-1);
+			start = i + 1;
+		}
+		if ((separate[i][0] == '|' || separate[i][0] == '>' \
+			|| separate[i][0] == '<') \
+			&& append_command_array(command_args, separate, i, i))
+			return (-1);
+	}
+	return (0);
+}
+
+static int	check_end_command(char **separate, int i, int *start)
+{
+	int	end;
+
+	end = -1;
+	if (separate[i][0] == '|' || separate[i][0] == '>' \
+		|| separate[i][0] == '<')
+		end = i - 1;
+	if (i > 0 && (separate[i - 1][0] == '|' || separate[i - 1][0] == '>' \
+		|| separate[i - 1][0] == '<'))
+	{
+		*start = i;
+		if (separate[i - 1][0] == '>' || separate[i - 1][0] == '<')
+			end = i;
+	}
+	if (!separate[i + 1])
+		end = i;
+	if (end != -1 && separate[end][0] == '|')
+		end--;
+	return (end);
+}
+
+// Agrega al final de el array command_args los strings
+// de los indices start y end de separate_line
+static int	append_command_array(char ***command_args, char **separate, \
+									int start, int end)
+{
+	char	**commands;
+	char	*aux;
+	int		i;
+
+	commands = *command_args;
+	i = 0;
+	while (commands[i])
+		i++;
+	commands[i] = ft_substr(separate[start], 0, ft_strlen(separate[start]));
+	while (++start <= end)
+	{
+		if (separate[start - 1][0] != '>' && separate[start - 1][0] != '<')
+			aux = ft_strjoin(commands[i], " ");
+		else
+			aux = ft_strjoin(commands[i], "");
+		if (!aux)
+			return (1);
+		free(commands[i]);
+		commands[i] = ft_strjoin(aux, separate[start]);
+		free(aux);
+		if (!commands[i])
+			return (1);
+	}
+	return (0);
+}
