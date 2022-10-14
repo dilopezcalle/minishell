8168525ef2a4b60f47ef24c947c6fe524a8d79f6
Diego Lopez calle <dilopez-@c4r1s6.42urduliz.com>
2022-10-14 18:31:16 +0200

    Status 127 done

--- src/executor.c
+++ src/executor.c
@@ -6,7 +6,7 @@
 /*   By: dilopez- <dilopez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 14:38:24 by dilopez-          #+#    #+#             */
-/*   Updated: 2022/10/14 18:19:12 by dilopez-         ###   ########.fr       */
+/*   Updated: 2022/10/14 18:27:20 by dilopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,7 @@ void	executor(t_command *commands, char **envp[])
 		g_exit_status = 130;
 	if (g_exit_status == 3)
 		g_exit_status = 131;
+	
 }
 
 static int	create_and_execute_child(t_command *commands, int fd[2], \
@@ -96,8 +97,9 @@ static void	execute_command(t_simple_command *command, int fd[2], \
 	close(fd[1]);
 	if (is_command_builtin(command->arguments[0]))
 		execute_child_builtin(command, envp);
+	if (!command->path)
+		exit (1);
 	execve(command->path, command->arguments, envp);
-	exit (1);
 }
 
 static int	parent_control(t_command *commands, int fd[2], int i, int pid)
